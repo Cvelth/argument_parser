2bb358ca654f57a809d330dbaf571037f2d312e7
Cvelth <Cvelth.mail@gmail.com>
2020-04-30 19:49:08 +0300

    Implement additional tests (#20)
    * Add single integer test
    * Redesign `value.cpp` structure
    * Add floating point value tests.
    * Move `constructible_from_*` objects to `argument_tester.hpp`

--- test/source/value.cpp
+++ test/source/value.cpp
@@ -1,37 +1,9 @@
 #include "argument_parser.hpp"
 #include "argument_tester.hpp"
 
-template <typename type>
-class constructible_from_type {
-public:
-	explicit constexpr constructible_from_type(type const &v) : value_(v) {}
-	constexpr bool operator==(constructible_from_type<type> const &o) const {
-		return value_ == o.value_;
-	}
-
-protected:
-	type value_;
-};
-using constructible_from_int		 = constructible_from_type<int>;
-using constructible_from_bool		 = constructible_from_type<bool>;
-using constructible_from_double		 = constructible_from_type<double>;
-using constructible_from_string		 = constructible_from_type<std::string>;
-using constructible_from_string_view = constructible_from_type<std::string_view>;
-class constructible_from_char_star : public constructible_from_type<char const *> {
-public:
-	using constructible_from_type<char const *>::constructible_from_type;
-	bool operator==(constructible_from_char_star const &o) const {
-		return !std::strcmp(value_, o.value_);
-	}
-};
-
-TEST_CASE("Arguments object with a single <-value> option.") {
-	using namespace ap;
-	arguments test_arguments{value{"value"}};
-
-	SUBCASE("Passing no arguments") {
-		// ./executable
-		argument_tester(parse_helper(test_arguments, {}))
+TEST_CASE("Arguments object with a single <-value> option and no input.") {
+	auto check = [](ap::arguments &test_arguments, std::initializer_list<std::string> &&inputs) {
+		ap::argument_tester(parse_helper(test_arguments, std::move(inputs)))
 			.no_errors()
 			.no_warnings()
 
@@ -73,11 +45,19 @@ TEST_CASE("Arguments object with a single <-value> option.") {
 
 			.argument_is_not_convertible_to<constructible_from_int>("value")
 			.argument_is_not_convertible_to<constructible_from_double>("value");
-	}
+	};
 
-	SUBCASE("Passing a single string argument") {
-		// ./executable -value="hello world"
-		argument_tester(parse_helper(test_arguments, {"-value=\"hello world\""}))
+	using namespace ap;
+	arguments test_arguments{value{"value"}};
+	SUBCASE("Passing no arguments") {
+		// ./executable
+		check(test_arguments, {});
+	}
+}
+
+TEST_CASE("Arguments object with a single <-value> option and string input.") {
+	auto check = [](ap::arguments &test_arguments, std::initializer_list<std::string> &&inputs) {
+		ap::argument_tester(parse_helper(test_arguments, std::move(inputs)))
 			.no_errors()
 			.no_warnings()
 
@@ -120,5 +100,283 @@ TEST_CASE("Arguments object with a single <-value> option.") {
 
 			.argument_is_not_convertible_to<constructible_from_int>("value")
 			.argument_is_not_convertible_to<constructible_from_double>("value");
+	};
+
+	using namespace ap;
+	arguments test_arguments{value{"value"}};
+	SUBCASE("using \"=\"") {
+		// ./executable -value="hello world"
+		check(test_arguments, {"-value=\"hello world\""});
+	}
+	SUBCASE("using \" \"") {
+		// ./executable -value "hello world"
+		check(test_arguments, {"-value", "hello world"});
+	}
+}
+
+TEST_CASE("Arguments object with a single <-value> option and integer input.") {
+	auto check = [](ap::arguments &test_arguments, std::initializer_list<std::string> &&inputs) {
+		ap::argument_tester(parse_helper(test_arguments, std::move(inputs)))
+			.no_errors()
+			.no_warnings()
+
+			.argument_is_convertible_to<bool>("value")
+			.argument_is_equal("value", true)
+
+			.argument_is_convertible_to<signed int>("value")
+			.argument_is_convertible_to<unsigned int>("value")
+			.argument_is_convertible_to<signed short>("value")
+			.argument_is_convertible_to<unsigned short>("value")
+			.argument_is_convertible_to<signed long>("value")
+			.argument_is_convertible_to<unsigned long>("value")
+			.argument_is_convertible_to<signed long long>("value")
+			.argument_is_convertible_to<unsigned long long>("value")
+			.argument_is_equal("value", 42)
+			.argument_is_equal("value", 42u)
+			.argument_is_equal("value", (signed short) 42)
+			.argument_is_equal("value", (unsigned short) 42)
+			.argument_is_equal("value", 42l)
+			.argument_is_equal("value", 42ul)
+			.argument_is_equal("value", 42ll)
+			.argument_is_equal("value", 42ull)
+
+			.argument_is_convertible_to<float>("value")
+			.argument_is_convertible_to<double>("value")
+			.argument_is_convertible_to<long double>("value")
+			.argument_is_equal("value", 42.f)
+			.argument_is_equal("value", 42.0)
+			.argument_is_equal("value", 42.0l)
+
+			.argument_is_convertible_to<unsigned char>("value")
+			.argument_is_convertible_to<signed char>("value")
+			.argument_is_equal("value", (signed char) 42)
+			.argument_is_equal("value", (unsigned char) 42)
+
+			.argument_is_convertible_to<char const *>("value")
+			.argument_is_not_convertible_to<char *>("value")  // by design
+			.argument_is_convertible_to<std::string>("value")
+			.argument_is_convertible_to<std::string_view>("value")
+			.argument_is_equal("value", "42")
+			.argument_is_equal("value", std::string{"42"})
+			.argument_is_equal("value", std::string_view{"42"})
+
+			.argument_is_convertible_to<constructible_from_bool>("value")
+			.argument_is_convertible_to<constructible_from_string>("value")
+			.argument_is_convertible_to<constructible_from_string_view>("value")
+			.argument_is_convertible_to<constructible_from_char_star>("value")
+			.argument_is_equal("value", constructible_from_bool{true})
+			.argument_is_equal("value", constructible_from_string{std::string{"42"}})
+			.argument_is_equal("value", constructible_from_string_view{std::string_view{"42"}})
+			.argument_is_equal("value", constructible_from_char_star{"42"})
+
+			.argument_is_convertible_to<constructible_from_int>("value")
+			.argument_is_convertible_to<constructible_from_double>("value")
+			.argument_is_equal("value", constructible_from_int{42})
+			.argument_is_equal("value", constructible_from_double{42.0});
+	};
+
+	using namespace ap;
+	arguments test_arguments{value{"value"}};
+	SUBCASE("using \"=\"") {
+		// ./executable -value=42
+		check(test_arguments, {"-value=42"});
+	}
+	SUBCASE("using \" \"") {
+		// ./executable -value 42
+		check(test_arguments, {"-value", "42"});
+	}
+}
+
+TEST_CASE("Arguments object with a single <-value> option and singed integer input.") {
+	auto check = [](ap::arguments &test_arguments, std::initializer_list<std::string> &&inputs) {
+		ap::argument_tester(parse_helper(test_arguments, std::move(inputs)))
+			.no_errors()
+			.no_warnings()
+
+			.argument_is_convertible_to<bool>("value")
+			.argument_is_equal("value", true)
+
+			.argument_is_convertible_to<signed int>("value")
+			.argument_is_convertible_to<signed short>("value")
+			.argument_is_convertible_to<signed long>("value")
+			.argument_is_convertible_to<signed long long>("value")
+			.argument_is_equal("value", -42)
+			.argument_is_equal("value", (signed short) -42)
+			.argument_is_equal("value", -42l)
+			.argument_is_equal("value", -42ll)
+
+			.argument_is_not_convertible_to<unsigned int>("value")
+			.argument_is_not_convertible_to<unsigned short>("value")
+			.argument_is_not_convertible_to<unsigned long>("value")
+			.argument_is_not_convertible_to<unsigned long long>("value")
+
+			.argument_is_convertible_to<float>("value")
+			.argument_is_convertible_to<double>("value")
+			.argument_is_convertible_to<long double>("value")
+			.argument_is_equal("value", -42.f)
+			.argument_is_equal("value", -42.0)
+			.argument_is_equal("value", -42.0l)
+
+			.argument_is_convertible_to<signed char>("value")
+			.argument_is_equal("value", (signed char) -42)
+
+			.argument_is_not_convertible_to<unsigned char>("value")
+
+			.argument_is_convertible_to<char const *>("value")
+			.argument_is_not_convertible_to<char *>("value")  // by design
+			.argument_is_convertible_to<std::string>("value")
+			.argument_is_convertible_to<std::string_view>("value")
+			.argument_is_equal("value", "-42")
+			.argument_is_equal("value", std::string{"-42"})
+			.argument_is_equal("value", std::string_view{"-42"})
+
+			.argument_is_convertible_to<constructible_from_bool>("value")
+			.argument_is_convertible_to<constructible_from_string>("value")
+			.argument_is_convertible_to<constructible_from_string_view>("value")
+			.argument_is_convertible_to<constructible_from_char_star>("value")
+			.argument_is_equal("value", constructible_from_bool{true})
+			.argument_is_equal("value", constructible_from_string{std::string{"-42"}})
+			.argument_is_equal("value", constructible_from_string_view{std::string_view{"-42"}})
+			.argument_is_equal("value", constructible_from_char_star{"-42"})
+
+			.argument_is_convertible_to<constructible_from_int>("value")
+			.argument_is_convertible_to<constructible_from_double>("value")
+			.argument_is_equal("value", constructible_from_int{-42})
+			.argument_is_equal("value", constructible_from_double{-42.0});
+	};
+
+	using namespace ap;
+	arguments test_arguments{value{"value"}};
+	SUBCASE("using \"=\"") {
+		// ./executable -value=-42
+		check(test_arguments, {"-value=-42"});
+	}
+	SUBCASE("using \" \"") {
+		// ./executable -value -42
+		check(test_arguments, {"-value", "-42"});
+	}
+}
+
+TEST_CASE("Arguments object with a single <-value> option and floating point input.") {
+	auto check = [](ap::arguments &test_arguments, std::initializer_list<std::string> &&inputs) {
+		ap::argument_tester(parse_helper(test_arguments, std::move(inputs)))
+			.no_errors()
+			.no_warnings()
+
+			.argument_is_convertible_to<bool>("value")
+			.argument_is_equal("value", true)
+
+			.argument_is_not_convertible_to<signed int>("value")
+			.argument_is_not_convertible_to<unsigned int>("value")
+			.argument_is_not_convertible_to<signed short>("value")
+			.argument_is_not_convertible_to<unsigned short>("value")
+			.argument_is_not_convertible_to<signed long>("value")
+			.argument_is_not_convertible_to<unsigned long>("value")
+			.argument_is_not_convertible_to<signed long long>("value")
+			.argument_is_not_convertible_to<unsigned long long>("value")
+
+			.argument_is_convertible_to<float>("value")
+			.argument_is_convertible_to<double>("value")
+			.argument_is_convertible_to<long double>("value")
+			.argument_is_equal("value", 4.2f)
+			.argument_is_equal("value", 4.2)
+			.argument_is_equal("value", 4.2l)
+
+			.argument_is_not_convertible_to<signed char>("value")
+			.argument_is_not_convertible_to<unsigned char>("value")
+
+			.argument_is_convertible_to<char const *>("value")
+			.argument_is_not_convertible_to<char *>("value")  // by design
+			.argument_is_convertible_to<std::string>("value")
+			.argument_is_convertible_to<std::string_view>("value")
+			.argument_is_equal("value", "4.2")
+			.argument_is_equal("value", std::string{"4.2"})
+			.argument_is_equal("value", std::string_view{"4.2"})
+
+			.argument_is_convertible_to<constructible_from_bool>("value")
+			.argument_is_convertible_to<constructible_from_string>("value")
+			.argument_is_convertible_to<constructible_from_string_view>("value")
+			.argument_is_convertible_to<constructible_from_char_star>("value")
+			.argument_is_equal("value", constructible_from_bool{true})
+			.argument_is_equal("value", constructible_from_string{std::string{"4.2"}})
+			.argument_is_equal("value", constructible_from_string_view{std::string_view{"4.2"}})
+			.argument_is_equal("value", constructible_from_char_star{"4.2"})
+
+			.argument_is_not_convertible_to<constructible_from_int>("value")
+			.argument_is_convertible_to<constructible_from_double>("value")
+			.argument_is_equal("value", constructible_from_double{4.2});
+	};
+
+	using namespace ap;
+	arguments test_arguments{value{"value"}};
+	SUBCASE("using \"=\"") {
+		// ./executable -value=4.2
+		check(test_arguments, {"-value=4.2"});
+	}
+	SUBCASE("using \" \"") {
+		// ./executable -value 4.2
+		check(test_arguments, {"-value", "4.2"});
+	}
+}
+
+TEST_CASE("Arguments object with a single <-value> option and signed floating point input.") {
+	auto check = [](ap::arguments &test_arguments, std::initializer_list<std::string> &&inputs) {
+		ap::argument_tester(parse_helper(test_arguments, std::move(inputs)))
+			.no_errors()
+			.no_warnings()
+
+			.argument_is_convertible_to<bool>("value")
+			.argument_is_equal("value", true)
+
+			.argument_is_not_convertible_to<signed int>("value")
+			.argument_is_not_convertible_to<unsigned int>("value")
+			.argument_is_not_convertible_to<signed short>("value")
+			.argument_is_not_convertible_to<unsigned short>("value")
+			.argument_is_not_convertible_to<signed long>("value")
+			.argument_is_not_convertible_to<unsigned long>("value")
+			.argument_is_not_convertible_to<signed long long>("value")
+			.argument_is_not_convertible_to<unsigned long long>("value")
+
+			.argument_is_convertible_to<float>("value")
+			.argument_is_convertible_to<double>("value")
+			.argument_is_convertible_to<long double>("value")
+			.argument_is_equal("value", -.42f)
+			.argument_is_equal("value", -.42)
+			.argument_is_equal("value", -.42l)
+
+			.argument_is_not_convertible_to<signed char>("value")
+			.argument_is_not_convertible_to<unsigned char>("value")
+
+			.argument_is_convertible_to<char const *>("value")
+			.argument_is_not_convertible_to<char *>("value")  // by design
+			.argument_is_convertible_to<std::string>("value")
+			.argument_is_convertible_to<std::string_view>("value")
+			.argument_is_equal("value", "-.42")
+			.argument_is_equal("value", std::string{"-.42"})
+			.argument_is_equal("value", std::string_view{"-.42"})
+
+			.argument_is_convertible_to<constructible_from_bool>("value")
+			.argument_is_convertible_to<constructible_from_string>("value")
+			.argument_is_convertible_to<constructible_from_string_view>("value")
+			.argument_is_convertible_to<constructible_from_char_star>("value")
+			.argument_is_equal("value", constructible_from_bool{true})
+			.argument_is_equal("value", constructible_from_string{std::string{"-.42"}})
+			.argument_is_equal("value", constructible_from_string_view{std::string_view{"-.42"}})
+			.argument_is_equal("value", constructible_from_char_star{"-.42"})
+
+			.argument_is_not_convertible_to<constructible_from_int>("value")
+			.argument_is_convertible_to<constructible_from_double>("value")
+			.argument_is_equal("value", constructible_from_double{-.42});
+	};
+
+	using namespace ap;
+	arguments test_arguments{value{"value"}};
+	SUBCASE("using \"=\"") {
+		// ./executable -value=-.42
+		check(test_arguments, {"-value=-.42"});
+	}
+	SUBCASE("using \" \"") {
+		// ./executable -value 4.2
+		check(test_arguments, {"-value", "-.42"});
 	}
 }
