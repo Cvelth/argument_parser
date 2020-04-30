fedb0f2bf6a2c1e71ff0e7d0232645f29b0c9119
cvelth <cvelth.mail@gmail.com>
2020-04-30 13:09:18 +0300

    Add single integer test

--- test/source/value.cpp
+++ test/source/value.cpp
@@ -121,4 +121,65 @@ TEST_CASE("Arguments object with a single <-value> option.") {
 			.argument_is_not_convertible_to<constructible_from_int>("value")
 			.argument_is_not_convertible_to<constructible_from_double>("value");
 	}
+
+	SUBCASE("Passing a single integer argument") {
+		// ./executable -value=42
+		argument_tester(parse_helper(test_arguments, {"-value=42"}))
+			.no_errors()
+			.no_warnings()
+
+			.argument_is_convertible_to<bool>("value")
+			.argument_is_equal("value", true)
+
+			.argument_is_convertible_to<signed int>("value")
+			.argument_is_convertible_to<unsigned int>("value")
+			.argument_is_convertible_to<signed short>("value")
+			.argument_is_convertible_to<unsigned short>("value")
+			.argument_is_convertible_to<signed long>("value")
+			.argument_is_convertible_to<unsigned long>("value")
+			.argument_is_convertible_to<signed long long>("value")
+			.argument_is_convertible_to<unsigned long long>("value")
+			.argument_is_equal("value", 42)
+			.argument_is_equal("value", 42u)
+			.argument_is_equal("value", (signed short) 42)
+			.argument_is_equal("value", (unsigned short) 42)
+			.argument_is_equal("value", 42l)
+			.argument_is_equal("value", 42ul)
+			.argument_is_equal("value", 42ll)
+			.argument_is_equal("value", 42ull)
+
+			.argument_is_convertible_to<float>("value")
+			.argument_is_convertible_to<double>("value")
+			.argument_is_convertible_to<long double>("value")
+			.argument_is_equal("value", 42.f)
+			.argument_is_equal("value", 42.0)
+			.argument_is_equal("value", 42.0l)
+
+			.argument_is_convertible_to<unsigned char>("value")
+			.argument_is_convertible_to<signed char>("value")
+			.argument_is_equal("value", (signed char) 42)
+			.argument_is_equal("value", (unsigned char) 42)
+
+			.argument_is_convertible_to<char const *>("value")
+			.argument_is_not_convertible_to<char *>("value")  // by design
+			.argument_is_convertible_to<std::string>("value")
+			.argument_is_convertible_to<std::string_view>("value")
+			.argument_is_equal("value", "42")
+			.argument_is_equal("value", std::string{"42"})
+			.argument_is_equal("value", std::string_view{"42"})
+
+			.argument_is_convertible_to<constructible_from_bool>("value")
+			.argument_is_convertible_to<constructible_from_string>("value")
+			.argument_is_convertible_to<constructible_from_string_view>("value")
+			.argument_is_convertible_to<constructible_from_char_star>("value")
+			.argument_is_equal("value", constructible_from_bool{true})
+			.argument_is_equal("value", constructible_from_string{std::string{"42"}})
+			.argument_is_equal("value", constructible_from_string_view{std::string_view{"42"}})
+			.argument_is_equal("value", constructible_from_char_star{"42"})
+
+			.argument_is_convertible_to<constructible_from_int>("value")
+			.argument_is_convertible_to<constructible_from_double>("value")
+			.argument_is_equal("value", constructible_from_int{42})
+			.argument_is_equal("value", constructible_from_double{42.0});
+	}
 }
