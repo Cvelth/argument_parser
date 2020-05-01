0c08a52068cd1707821cd151ebb0be8adcc6a9d8
cvelth <cvelth.mail@gmail.com>
2020-05-01 16:24:58 +0300

    Fix style.

--- test/source/counter.cpp
+++ test/source/counter.cpp
@@ -63,8 +63,8 @@ TEST_CASE("Arguments object with a single <-counter> argument.") {
 
 	SUBCASE("Repeating the call 7 times") {
 		// ./executable -counter -counter --counter -x -c -c -x
-		ap::argument_tester(
-			parse_helper(test_arguments, {"-counter", "-counter", "--counter", "-x", "-c", "-c", "-x"}))
+		ap::argument_tester(parse_helper(test_arguments, {"-counter", "-counter", "--counter", "-x",
+														  "-c", "-c", "-x"}))
 			.no_errors()
 			.no_warnings()
 
